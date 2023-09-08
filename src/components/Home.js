c76951c4578582f317690dd5b67af70edbc6723a
surajkumar22079 <90059548+surajkumar22079@users.noreply.github.com>
2023-09-08 23:45:58 +0530

    first commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
+          
             <Section
               title="Model S"
               description="Order Online for Touchless Delivery"
